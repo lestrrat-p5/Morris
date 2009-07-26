3b1c2d62c295814e4fae911bebbe58553f9cc855
lestrrat <lestrrat+github@gmail.com>
2009-07-26 23:29:14 +0900

    don't end with a connector

--- lib/Morris/Plugin/Channel/QOTD.pm
+++ lib/Morris/Plugin/Channel/QOTD.pm
@@ -2,18 +2,30 @@
 
 package Morris::Plugin::Channel::QOTD;
 use Moose;
+use Moose::Util::TypeConstraints;
+use Text::MeCab;
+use namespace::clean -except => qw(meta);
 
 with 'Morris::Plugin';
 
-has 'command' => (
+coerce 'RegexpRef'
+    => from 'Str'
+    => via { qr/$_/ }
+;
+
+has command => (
     is => 'rw',
-    isa => 'Str | RegexpRef',
-    default => 'dan'
+    isa => 'RegexpRef',
+    default => sub { qr/dan/ },
+    coerce => 1,
 );
 
-__PACKAGE__->meta->make_immutable;
-
-no Moose;
+has connect_info => (
+    is => 'ro',
+    isa => 'ArrayRef',
+    coerce => 1,
+    required => 1,
+);
 
 my @facemarks = (
     '( ´・ω・｀)',
@@ -30,6 +42,19 @@ my @facemarks = (
 sub register {
     my ($self, $conn) = @_;
     $conn->register_hook( 'channel.public', sub { $self->handle_message(@_) } );
+
+    my $dbh = DBI->connect(@{ $self->connect_info });
+    $dbh->{RaiseError} = 1;
+    $dbh->do(<<EOSQL);
+        CREATE TABLE IF NOT EXISTS qotd (
+            id integer auto_increment primary key,
+            channel text not null,
+            quote text not null,
+            ends_with_connector BOOLEAN NOT NULL DEFAULT 0,
+            created_on integer not null
+        );
+EOSQL
+
 }
 
 sub handle_message {
@@ -63,7 +88,7 @@ sub handle_message {
             $}x
      ) {
         # how many?
-        my $dbh = DBI->connect('dbi:SQLite:dbname=/service/morris/qotd.db');
+        my $dbh = DBI->connect(@{ $self->connect_info });
         if ($2) {
             $quote = $3;
             my $rv = $dbh->do("DELETE FROM qotd WHERE channel = ? AND quote = ?",
@@ -78,6 +103,7 @@ sub handle_message {
             }
         } elsif ($3) {
             $quote = $3;
+            my $ends_with_connector = $self->ends_with_connector($quote);
             $dbh->do("INSERT INTO qotd (channel, quote, created_on) VALUES (?, ?, ?)", undef, $channel, $quote, time());
 
             my $sth = $dbh->prepare("SELECT count(*) FROM qotd WHERE channel = ?");
@@ -89,24 +115,54 @@ sub handle_message {
         } else {
             my $x = $1;
             my $count = scalar(my @a = ($x =~ /\G((?:$command))/g));
-            my $sth = $dbh->prepare( "SELECT quote FROM qotd WHERE channel = ? ORDER BY random() LIMIT $count");
-            if ($sth->execute($channel)) {
-                $quote = '';
-                my $a_quote;
-                $sth->bind_columns(\$a_quote);
-                while ($sth->fetchrow_arrayref) {
-                    $quote .= $a_quote . ' ';
+
+            my $last = $dbh->prepare( "SELECT quote FROM qotd WHERE channel = ? AND ends_with_connector = 0 ORDER BY random() LIMIT 1");
+            if ($count == 1) {
+                $last->execute($channel);
+                ($quote) = $last->fetchrow_array();
+                $last->finish;
+            } else {
+                my $sth = $dbh->prepare( "SELECT quote FROM qotd WHERE channel = ? ORDER BY random() LIMIT " . ($count - 1));
+                if ($sth->execute($channel)) {
+                    $quote = '';
+                    my $a_quote;
+                    $sth->bind_columns(\$a_quote);
+                    while ($sth->fetchrow_arrayref) {
+                        $quote .= $a_quote . ' ';
+                    }
+                    $last->execute($channel);
+                    $quote .= ($last->fetchrow_array)[0];
+                    $last->finish;
                 }
+                $sth->finish;
             }
-            $sth->finish;
-            $dbh->disconnect;
         }
 
+        $dbh->disconnect;
         $self->connection->irc_notice({
             channel => $channel,
             message => $facemarks[ rand @facemarks ] . " < $quote ",
         });
     }
 }
+
+sub ends_with_connector {
+    my ($self, $quote) = @_;
+    my $mecab = Text::MeCab->new();
+
+    my $ret = 0;
+    my $prev;
+    for( my $node = $mecab->parse($quote); $node; $node = $node->next ) {
+        my $type = (split /,/, $node->feature)[0];
+        if ($type =~ /[EB]OS/ && ($prev eq '接続詞' || $prev eq '助詞')) {
+            $ret = 1;
+        }
+        $prev = $type;
+    }
+    return $ret;
+}
+
+__PACKAGE__->meta->make_immutable;
+
 
 1;
