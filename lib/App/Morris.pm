e3b678f77a9024e8fd2ae37e21e399486b4d03a8
lestrrat <lestrrat+github@gmail.com>
2009-11-10 14:42:28 +0900

    with the new SimpleConfig, we don't need this stuff

--- lib/App/Morris.pm
+++ lib/App/Morris.pm
@@ -4,7 +4,7 @@ use Config::Any;
 use Morris;
 use namespace::clean -except => qw(meta);
 
-with qw(MooseX::Getopt MooseX::ConfigFromFile);
+with qw(MooseX::Getopt MooseX::SimpleConfig);
 
 has '+configfile' => (
     default => '/etc/morris.conf'
@@ -30,27 +30,6 @@ sub config_any_args {
             }
         }
     };
-}
-
-sub get_config_from_file {
-    my ($class, $file) = @_;
-
-    my $raw_cfany = Config::Any->load_files({
-        %{ $class->config_any_args || {} },
-        files => [ $file ],
-        use_ext => 1,
-    });
-
-    die q{Specified configfile '} . $file
-        . q{' does not exist, is empty, or is not readable}
-            unless $raw_cfany->[0]
-                && exists $raw_cfany->[0]->{$file};
-
-    my $raw_config = $raw_cfany->[0]->{$file};
-    die "configfile must represent a hash structure"
-        unless $raw_config && ref $raw_config && ref $raw_config eq 'HASH';
-
-    $raw_config;
 }
 
 
